3d88142d5d9ede35ce6113ef9a6a33d441fe1a2f
ankitgoelmalviyans <157928570+ankitgoelmalviyans@users.noreply.github.com>
2025-04-23 23:43:48 +0530

    Create environment.prod.ts

---
+++ BookStore.ProductSCA/product-ui/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    apiBaseUrl: 'https://bookstoreproductservice.azurewebsites.net/api',
+    inventoryApiUrl: 'https://bookstoreinventoryserviceapi20250418125639.azurewebsites.net/api',
+    //authUrl: 'https://localhost:57796/api'
+    authUrl: 'https://bookauthservice.azurewebsites.net/api'
+  };
