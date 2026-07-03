eb85fecf256293b0aa30882761e63657e3dea2d8
ankitgoelmalviyans <ankitmalviyans@gmail.com>
2026-07-03 17:16:46 +0000

    Wire ProductService to Cosmos DB

--- BookStore.ProductSCA/BookStore.ProductService/src/BookStore.ProductService.API/StartupExtensions.cs
+++ BookStore.ProductSCA/BookStore.ProductService/src/BookStore.ProductService.API/StartupExtensions.cs
@@ -1,13 +1,11 @@
 using BookStore.ProductService.API.Middleware;
 using BookStore.ProductService.Core.Repositories;
-using BookStore.ProductService.Infrastructure.Data;
 using BookStore.ProductService.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
 using BookStore.ProductService.Application.Interfaces;
 using BookStore.ProductService.Core.Messaging;
 using Azure.Messaging.ServiceBus;
-using Infrastructure.Messaging;
+using Microsoft.Azure.Cosmos;
 
 
 namespace BookStore.ProductService.Extensions
@@ -16,55 +14,13 @@ namespace BookStore.ProductService.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddDbContext<BookStoreDbContext>(options =>
-    options.UseInMemoryDatabase("ProductDb"));
+            services.AddSingleton<CosmosClient>(sp => new CosmosClient(
+                config["CosmosDb:CosmosEndpoint"],
+                config["CosmosDb:AccountKey"]
+            ));
 
-
-
-            services.AddDbContext<BookStoreDbContext>((serviceProvider, options) =>
-            {
-                var config = serviceProvider.GetRequiredService<IConfiguration>();
-                var provider = config["Database:Provider"];
-
-                if (provider == "Cosmos")
-                {
-                    options.UseCosmos(
-                        config["Database:Cosmos:AccountEndpoint"],
-                        config["Database:Cosmos:AccountKey"],
-                        config["Database:Cosmos:DatabaseName"]
-                    );
-                }
-                else if (provider == "SqlServer")
-                {
-                    //options.UseSqlServer(config["Database:SqlServer:ConnectionString"]);
-                }
-                else if (provider == "InMemory")
-                {
-                    options.UseInMemoryDatabase("ProductDb");
-                }
-            });
-
-
-
-
-
-
-
-
-            //services.AddDbContext<BookStoreDbContext>(options =>
-            //{
-            //    options.UseCosmos(
-            //        config["CosmosDb:AccountEndpoint"],     // e.g. https://your-db.documents.azure.com
-            //        config["CosmosDb:AccountKey"],          // Secret key
-            //        databaseName: config["CosmosDb:DatabaseName"]
-            //    );
-            //});
-
-
-
-            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductRepository, CosmosProductRepository>();
             services.AddScoped<IProductService, BookStore.ProductService.Application.Services.ProductService>();
-
 
             services.AddScoped<IMessagePublisher, AzureServiceBusProducer>();
 
